8c0eea2f5b554822c9edd845bf677c7f979eb6ec
CesatSoftware2 <software2@cesat.mx>
2024-02-13 08:32:11 -0800

    Agregar archivos de proyecto.

---
+++ Camera_Prueba/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Camera_Prueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer_camera = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numeric_programa = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_programa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Trigger";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label1.Location = new System.Drawing.Point(359, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "OK";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(22, 144);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(369, 240);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer_camera
+            // 
+            this.timer_camera.Enabled = true;
+            this.timer_camera.Interval = 20;
+            this.timer_camera.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(230, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(230, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 59);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Set Program";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numeric_programa
+            // 
+            this.numeric_programa.Location = new System.Drawing.Point(233, 47);
+            this.numeric_programa.Name = "numeric_programa";
+            this.numeric_programa.Size = new System.Drawing.Size(120, 20);
+            this.numeric_programa.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 396);
+            this.Controls.Add(this.numeric_programa);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_programa)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer_camera;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numeric_programa;
+    }
+}
+
---
+++ Camera_Prueba/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Keyence.IV3.Sdk;
+
+namespace Camera_Prueba
+{
+    public partial class Form1 : Form
+    {
+        Size image_size = new Size(320, 240);
+        VisionSensorStore store = new VisionSensorStore();
+        IVisionSensor camera;
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                byte[] ipAddressLocal = { 192, 168, 0, 11 };
+                IPAddress ipLocal = new IPAddress(ipAddressLocal);
+                VisionSensorStore.StartPoint = ipLocal;
+                
+                byte[] ipAddressCamera = { 192, 168, 0, 178 };
+                IPAddress ipCamera = new IPAddress(ipAddressCamera);
+                camera = store.Create(ipCamera, 63000);
+                
+                camera.EventEnable = true;
+                camera.ResultUpdated += EventoResultadoCamera;
+                camera.ImageAcquired += EventoImagenCamera;
+
+                timer_camera.Start();
+            }
+            catch 
+            { 
+            }
+
+            
+        }
+
+        private void EventoResultadoCamera(object sender, ToolResultUpdatedEventArgs e )
+        {
+            if (e.TotalStatusResult)
+            {
+                label1.Text = "OK";
+                label1.ForeColor = Color.Lime;
+            }
+            else
+            {
+                label1.Text = "NOK";
+                label1.ForeColor = Color.Red;
+            }
+        }
+
+        private void EventoImagenCamera(object sender, ImageAcquiredEventArgs e)
+        {
+            var foto = new Bitmap(image_size.Width, image_size.Height, PixelFormat.Format24bppRgb);
+            BitmapData bitData = foto.LockBits(new Rectangle(Point.Empty, image_size), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+            Marshal.Copy(e.LiveImage.ByteData, 0, bitData.Scan0, e.LiveImage.ByteData.Length);
+            foto.UnlockBits(bitData);
+
+            using (Graphics Marcador = Graphics.FromImage(foto))
+            {
+                Marcador.SmoothingMode = SmoothingMode.AntiAlias;
+                for (byte i = 0; i < 16; i++)
+                {
+                    camera.DrawWindow(Marcador,Color.Green,Color.Red,i);
+                }
+            }
+
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Image.Dispose();
+            }
+
+            pictureBox1.Image = foto;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            camera.TickTack();
+            label2.Text = camera.ActiveProgram.ProgramName;
+            if(camera.Errors.Length > 0)
+            {
+                label3.Text = camera.Errors[0].Description;
+                label3.Visible = true;
+
+                camera.ClearError(camera.Errors[0]);
+            }
+            else
+            {
+                if(label3.Visible)
+                {
+                    label3.Text = "";
+                    label3.Visible = false;
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            camera.Trigger();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ProgramHeader[] programaDisponibles = camera.Programs;
+            if ((int)numeric_programa.Value < programaDisponibles.Length && !camera.ExternalProgramSwitch) 
+            {
+                try
+                {
+                    camera.SwitchProgramTo(programaDisponibles[(int)numeric_programa.Value]);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+    }
+}
